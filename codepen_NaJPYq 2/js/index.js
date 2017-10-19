ab200ae03efeb10c56a07917ef306cb155270e0c
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-10-19 19:42:25 -0400

    Add files via upload

---
+++ codepen_NaJPYq 2/js/index.js
@@ -0,0 +1,24 @@
+$("#button1").click(function() {
+  $('.box').toggleClass('box-visible');
+});
+
+$("#settingsGear").click(function() {
+  window.setTimeout(function() {
+    var e = document.getElementById("settingsOption");
+    if(e.style.display == null || e.style.display == "none") {
+        e.style.display = "block";
+    } else {
+        e.style.display = "none";
+    }
+  }, 2000)
+  $('.settings').toggleClass('show-settings');
+});
+
+function showHideDiv(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == null || e.style.display == "none") {
+        e.style.display = "block";
+    } else {
+        e.style.display = "none";
+    }
+}
