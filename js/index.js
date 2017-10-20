90509a3069dd74f392921e97c265777a8f31a869
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-10-20 17:28:35 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,13 +6,25 @@ $("#settingsGear").click(function() {
   $('.settings').toggleClass('show-settings');
   
   if($('.settings').hasClass('show-settings')) {
-    //window.setTimeout(function() {
     var e = document.getElementById("settingsOption");
     e.style.display = "block";
-  //}, 100)
   }
   else {
     var e = document.getElementById("settingsOption");
     e.style.display = "none";
   }
 });
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.settings')) {
+    var d = document.getElementsByClassName("settingsOption");
+    
+    for (i = 0; i < d.length; i++) {
+      var di = d[i];
+      if (di.style.display != "block";) {
+        di.style.display = "block";
+      }
+    }
+  }
+}
