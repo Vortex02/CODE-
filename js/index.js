210f4f5fa223c260cf90e1c458fdc7ec523587b8
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-11-01 09:34:03 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,3 @@
 function showButtonClick() {
   $('.box').toggleClass('box-visible');
 }
-
-function settingsGearClick() {
-  $('.settings').toggleClass('show-settings');
-  
-  if($('.settings').hasClass('show-settings')) {
-    window.setTimeout(function() {
-      var e = document.getElementById("settingsOption");
-      e.style.display = "block";
-    }, 1000)
-  }
-}
-
-
-window.onclick = function(event) {
-  if (!event.target.matches('.settings')) {
-    var d = document.getElementsByClassName("settingsOption");
-    
-    for (var i = 0; i < d.length; i++) {
-      var di = d[i];
-      if (di.style.display != "none";) {
-        di.style.display = "none";
-      }
-    }
-  }
-}
